13d0e7020a92cb959f8a1a78c344667bf2308bc1
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2018-03-07 16:26:20 -0300

    adicionando cena de Resultado

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Router, Scene } from 'react-native-router-flux';
 import Principal from './src/components/Principal';
 import SobreJogo from './src/components/SobreJogo';
 import OutrosJogo from './src/components/OutrosJogos';
+import Resultado from './src/components/Resultado';
 
 export default class App extends Component {
   render() {
@@ -15,6 +16,7 @@ export default class App extends Component {
           <Scene key='principal' component={Principal} initil title="Cara ou Coroa" />
           <Scene key='sobrejogo' component={SobreJogo} title="Sobre o Jogo" />
           <Scene key='outrosjogos' component={OutrosJogo} title="Outros Jogos" />
+          <Scene key='resultado' component={Resultado} title="Resultado" />
         </Scene>
       </Router>
     );
