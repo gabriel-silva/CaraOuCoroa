c3c401e68c078736fd7138cc3fbe8014847c5552
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2018-03-07 16:35:31 -0300

    terminando o jogo CaraOuCoroa

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -10,13 +10,42 @@ const cara = require('../imgs/moeda_cara.png');
 const coroa = require('../imgs/moeda_coroa.png');
 
 export default class Resultado extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { resultado: '' };
+    }
+
+    UNSAFE_componentWillMount() {
+        const numAletorio = Math.floor(Math.random() * 2);
+        let caraOuCoroa = '';
+        if (numAletorio === 0) {
+            caraOuCoroa = 'cara';
+        } else {
+            caraOuCoroa = 'coroa';
+        }
+
+        //atualizando o setState com um obejto litral
+        this.setState({ resultado: caraOuCoroa });
+
+    }
+
     render() {
-        return (
-            <View style={styles.resultado}>
-                <Image source={cara} />
-                <Image source={coroa} />
-            </View>
-        );
+
+        if (this.state.resultado === 'cara') {
+            return (
+                <View style={styles.resultado}>
+                    <Image source={cara} />
+                </View>
+            );
+        }
+        else {
+            return (
+                <View style={styles.resultado}>
+                    <Image source={coroa} />
+                </View>
+            );
+        }
     }
 }
 
